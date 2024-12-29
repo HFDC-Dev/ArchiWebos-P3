87c0c7a6fb8e00a8bba0eda69842c10c5fc44166
HFDC-Dev <henriquedacosta.pro@gmail.com>
2024-12-29 14:14:47 +0100

    Récuperation des données API

--- FrontEnd/scripts/categories.js
+++ FrontEnd/scripts/categories.js
@@ -1,4 +1,15 @@
-const urlCategories = 'http://localhost:5678/api/categories'
-
-fetch(urlCategories)
-.then (response => response.json())
+// Utilisation de fetch pour récupérer des données depuis une API
+fetch('http://localhost:5678/api/categories') // Remplacez l'URL par l'URL de votre API
+  .then(response => {
+    if (!response.ok) {
+      // Si la réponse n'est pas OK (status 200-299)
+      throw new Error('Erreur réseau');
+    }
+    return response.json(); // Convertit la réponse en JSON
+  })
+  .then(data => {
+    console.log(data); // Affiche les données récupérées dans la console
+  })
+  .catch(error => {
+    console.error('Erreur:', error); // Affiche l'erreur si quelque chose ne va pas
+  });
--- FrontEnd/scripts/gallery.js
+++ FrontEnd/scripts/gallery.js
@@ -1,4 +1,30 @@
-const urlGallery = 'http://localhost:5678/api/works'
+// Utilisation de fetch pour récupérer des données depuis une API
+fetch('http://localhost:5678/api/works') // Remplacez l'URL par l'URL de votre API
+  .then(response => {
+    if (!response.ok) {
+      // Si la réponse n'est pas OK (status 200-299)
+      throw new Error('Erreur réseau');
+    }
+    return response.json(); // Convertit la réponse en JSON
+  })
+  .then(data => {
+    console.log(data); // Affiche les données récupérées dans la console
+    data.forEach((data, i) => {
+        setFigure(`Élément ${i}:`,data)
+    })
+  })
+  .catch(error => {
+    console.error('Erreur:', error); // Affiche l'erreur si quelque chose ne va pas
+  });
 
-fetch(urlGallery)
-.then (response => response.json())
+
+  
+  function setFigure(data) {
+    const figure = document.createElement("figure")
+  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+					<figcaption>${data.title}</figcaption>`;
+
+  document.body.append(figure);
+
+  }
+
