f1f89471fad253d9dd5f450d40df39644c176f69
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-01-07 04:32:46 +0100

    Récupération des catégories et ajout d'un bouton 'Tous'

--- FrontEnd/scripts/categories.js
+++ FrontEnd/scripts/categories.js
@@ -1,15 +1,22 @@
-// Utilisation de fetch pour récupérer des données depuis une API
-fetch('http://localhost:5678/api/categories') // Remplacez l'URL par l'URL de votre API
-  .then(response => {
-    if (!response.ok) {
-      // Si la réponse n'est pas OK (status 200-299)
-      throw new Error('Erreur réseau');
-    }
-    return response.json(); // Convertit la réponse en JSON
-  })
-  .then(data => {
-    console.log(data); // Affiche les données récupérées dans la console
-  })
-  .catch(error => {
-    console.error('Erreur:', error); // Affiche l'erreur si quelque chose ne va pas
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const categoriesContainer = document.getElementById("categories");
+  const galleryContainer = document.querySelector(".gallery");
+  let allWorks = []; // Variable pour stocker tous les projets
+
+  // Récupération des catégories (utilisation de fetch pour récupérer les données depuis une API)
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des catégories");
+      }
+      return response.json();
+    })
+    .then((categories) => {
+      // Ajout d'un bouton "Tous"
+      const allButton = document.createElement("button");
+      allButton.className = "btn";
+      allButton.textContent = "Tous";
+      allButton.addEventListener("click", () => displayWorks(allWorks));
+      categoriesContainer.appendChild(allButton);
+    })
+})
