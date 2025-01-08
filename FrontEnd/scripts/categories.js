b5251ebd502fdd4371ca0de320c84cf077c5c6b7
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-01-08 21:17:04 +0100

    Supression du fichier 'gallery.js', récupération des images via l'api et tri dynamique de la galerie dans le fichier 'categories.js'

--- FrontEnd/scripts/categories.js
+++ FrontEnd/scripts/categories.js
@@ -35,5 +35,30 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Erreur lors du chargement des catégories:", error))
 
+  // Récupération des images(works) et les afficher
+  fetch("http://localhost:5678/api/works") // Url de l'api works
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la récupération des images");
+      }
+      return response.json();
+    })
+    .then((works) => {
+      allWorks = works; // Stocker dans une variable globale
+      displayWorks(allWorks); // Afficher toutes les images par défaut
+    })
+    .catch((error) => console.error("Erreur lors du chargement des images:", error));
 
-})
+  // Fonction pour afficher les images dans la galerie
+  function displayWorks(works) {
+    galleryContainer.innerHTML = ""; // Effacer la galerie existante
+    works.forEach((work) => {
+      const figure = document.createElement("figure");
+      figure.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption>${work.title}</figcaption>
+      `;
+      galleryContainer.appendChild(figure);
+    });
+  }
+});
