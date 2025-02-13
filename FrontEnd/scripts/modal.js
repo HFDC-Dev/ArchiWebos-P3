664cd3dd03cb4ad3adbda464391f4e07e0c51c34
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-02-14 00:20:42 +0100

    Ajout de la galerie dans la modal

--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    const pictureContainer = document.getElementById("picture")
     // Sélection du conteneur
     const modalContainer = document.querySelector(".modal-container");
     // Sélection de tous les éléments qui déclenchent l'ouverture de la modal 
@@ -10,5 +11,31 @@ window.addEventListener("DOMContentLoaded", () => {
     // Fonction pour afficher et masquer la modal
     function toggleModal() {
         modalContainer.classList.toggle("active");
+    }
+
+    fetch("http://localhost:5678/api/works") // Url de l'api works
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Erreur lors de la récupération des images");
+            }
+            return response.json();
+        })
+        .then((works) => {
+            allWorks = works; // Stocker dans une variable globale
+            displayWorks(allWorks); // Afficher toutes les images par défaut
+        })
+        .catch((error) => console.error("Erreur lors du chargement des images:", error));
+
+    // Fonction pour afficher les images dans la galerie
+    function displayWorks(works) {
+        pictureContainer.innerHTML = ""; // Effacer la galerie existante
+        works.forEach((work) => {
+            const figure = document.createElement("figure");
+            figure.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption></figcaption>
+      `;
+            pictureContainer.appendChild(figure);
+        });
     }
 });
